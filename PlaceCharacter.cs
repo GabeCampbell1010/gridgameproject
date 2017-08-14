18da9c7351288f809076fc256aa5567f367ff7b9
Scott <scottruhnau88@gmail.com>
2017-08-13 23:07:34 -0700

    Example style changes

--- PlaceCharacter.cs
+++ PlaceCharacter.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,24 +6,20 @@ using UnityEngine.SceneManagement;
 public class PlaceCharacter : MonoBehaviour
 {
     private GameObject character;
+	private GameObject firstEnemy;
+    private bool canPlace;
+	private GameObject floor;
+    private Grid grid;
+	private IEnumerator coroutine;
+	private IEnumerator coroutineRevert;
 
-    
-
+	public float speed = 10f;
+    public Vector2 pos;
+		
     public static int maxCharacters = 4; 
     public static int characterNum = 0; 
-
-    private GameObject firstEnemy;
-
-    private bool canPlace;
-
-    public float speed = 10f;
-    public Vector2 pos;
-
-    private GameObject floor;
-    private Grid grid;
-
-    public static GameObject[] characters;
-
+	public static GameObject[] characters;
+   
     void Start()
     {
         character = gameObject;
@@ -33,14 +29,12 @@ public class PlaceCharacter : MonoBehaviour
         firstEnemy = GameObject.Find("M1");
         character.transform.GetChild(0).GetComponent<Renderer>().enabled = true;
 
-        //added
         characters = new GameObject[maxCharacters];
         for (int i = 0; i < characters.Length; i++)
         {
             characters[i] = GameObject.Find("Character" + (i + 1));
             
         }
-        //end added
     }
 
     void Update()
@@ -55,22 +49,19 @@ public class PlaceCharacter : MonoBehaviour
     {
         for (int i = 0; i < grid.gridWidth; i++) {
             for (int j = 0; j < grid.gridHeight; j++) {
-                foreach (GameObject character in characters)
-                    if (new Vector2(character.transform.position.x, character.transform.position.y) == new Vector2(i, j))
+                foreach (GameObject character in characters) {
+                    if (new Vector2(character.transform.position.x, character.transform.position.y) == new Vector2(i, j)) {
                         if (character != characters[characterNum])
                         {
                             if (character.transform.GetComponent<Renderer>().enabled == true)
                             {
-                                
                                 grid.taken[i, j] = 1;//0 means its not taken, 1 means its taken
                             }
                             else 
                             {
-                                
                                 grid.taken[i, j] = 0;
                             }
-                        }
-                        
+                        }                        
                     }
                 }
             }
@@ -79,7 +70,6 @@ public class PlaceCharacter : MonoBehaviour
 
     void CharacterMove()
     {
-
         if (new Vector3(pos.x, pos.y, 0) == transform.position)
         {
             if (Input.GetButton("MoveUp") && pos.y < grid.gridHeight)
@@ -93,10 +83,10 @@ public class PlaceCharacter : MonoBehaviour
 
             else if (Input.GetButton("MoveRight") && pos.x < 2)
                 pos.x += 1;
-
         }
         transform.position = Vector2.MoveTowards(transform.position, pos, speed * Time.deltaTime);
     }
+	
     void CheckSpot()
     {
         int tx = (int)Mathf.Round(pos.x);
@@ -139,68 +129,48 @@ public class PlaceCharacter : MonoBehaviour
             character.transform.GetChild(1).GetComponent<Renderer>().enabled = true;
             UndoCharacterPlace();
         }
-
     }
 
     void UndoCharacterPlace()
-    {//supposed to be a reverse version of place next character
+    {
+		//supposed to be a reverse version of place next character
+		characterNum--;//changed to --   
+		characters[characterNum + 1].GetComponent<PlaceCharacter>().enabled = false;
+		characters[characterNum + 1].GetComponent<Renderer>().enabled = false;
+		characters[characterNum + 1].transform.GetChild(0).GetComponent<Renderer>().enabled = false;
+		characters[characterNum + 1].transform.GetChild(1).GetComponent<Renderer>().enabled = false;
+		
+		int tx = (int)Mathf.Round(characters[characterNum].transform.position.x);
+		int ty = (int)Mathf.Round(characters[characterNum].transform.position.y);
+		
+		grid.taken[tx, ty] = 0;
 
-            characterNum--;//changed to --   
-            characters[characterNum + 1].GetComponent<PlaceCharacter>().enabled = false;
-            characters[characterNum + 1].GetComponent<Renderer>().enabled = false;
-            characters[characterNum + 1].transform.GetChild(0).GetComponent<Renderer>().enabled = false;
-            characters[characterNum + 1].transform.GetChild(1).GetComponent<Renderer>().enabled = false;
+		canPlace = true;
 
-            
-            int tx = (int)Mathf.Round(characters[characterNum].transform.position.x);
-            int ty = (int)Mathf.Round(characters[characterNum].transform.position.y);
-            
-            grid.taken[tx, ty] = 0;
-
-            canPlace = true;
-
-            characters[characterNum].GetComponent<Renderer>().enabled = true;
-            characters[characterNum].GetComponent<PlaceCharacter>().enabled = true;
-
+		characters[characterNum].GetComponent<Renderer>().enabled = true;
+		characters[characterNum].GetComponent<PlaceCharacter>().enabled = true;
     }
 
     void PlaceNextCharacter()
     {
-
-        //added
         if (characterNum < (maxCharacters - 1)) {
-            
-            characterNum++;
-            
-            characters[characterNum - 1].GetComponent<PlaceCharacter>().enabled = false;
-            
+            characterNum++;            
+            characters[characterNum - 1].GetComponent<PlaceCharacter>().enabled = false;            
             characters[characterNum].GetComponent<Renderer>().enabled = true;
             characters[characterNum].GetComponent<PlaceCharacter>().enabled = true;
-
-            
-
         }
         else if (characterNum == (maxCharacters - 1))
         {
-            characters[maxCharacters - 1].GetComponent<PlaceCharacter>().enabled = false;
-            
-
+            characters[maxCharacters - 1].GetComponent<PlaceCharacter>().enabled = false;            
             coroutineRevert = WaitForReversionCall(KeyCode.U);
             StartCoroutine(coroutineRevert);
-
-            coroutine = WaitForKeyDown(KeyCode.Return);//
-            StartCoroutine(coroutine);
-
-            
+            coroutine = WaitForKeyDown(KeyCode.Return);
+            StartCoroutine(coroutine);            
         }
-        //end added
     }
 
-
-    private IEnumerator coroutineRevert;
     public IEnumerator WaitForReversionCall(KeyCode keyCode)
     {
-
         while (!Input.GetKeyDown(keyCode))
             yield return null;
         character = GameObject.Find("Character" + (maxCharacters));
@@ -208,14 +178,10 @@ public class PlaceCharacter : MonoBehaviour
         character.transform.GetChild(1).GetComponent<Renderer>().enabled = true;
         
         UnDoLastCharacterPlace();
-        
-
     }
 
-    private IEnumerator coroutine;
     public IEnumerator WaitForKeyDown(KeyCode keyCode)
     {
-        
         while (!Input.GetKeyDown(keyCode))
             yield return null;
         
@@ -226,14 +192,11 @@ public class PlaceCharacter : MonoBehaviour
 
     void UnDoLastCharacterPlace()
     {
-        
         int tx = (int)Mathf.Round(characters[characterNum].transform.position.x);
         int ty = (int)Mathf.Round(characters[characterNum].transform.position.y);
         
         grid.taken[tx, ty] = 0;
-
         canPlace = true;
-
         characters[characterNum].GetComponent<Renderer>().enabled = true;
         characters[characterNum].GetComponent<PlaceCharacter>().enabled = true;
     }
